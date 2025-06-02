65f5ee58163de3f731a565974ce2c13eabc38e23
daynyus02 <daynyus.halinauskas02@gmail.com>
2025-06-02 19:04:24 +0100

    testing pylint and pytest

--- dashboard/test_main.py
+++ dashboard/test_main.py
@@ -6,3 +6,8 @@ from main import hello_world
 def test_hello_world():
     """Testing if the function returns the correct message."""
     assert hello_world() == "Hello World!"
+
+
+def test_hello_world_fail():
+    """Testing if the function returns the correct message."""
+    assert hello_world() == ""
