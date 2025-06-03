9dc77a34f25050f52b594cf295f4e64c1b620f87
kcw36 <kylewat2000@hotmail.com>
2025-06-03 18:25:34 +0100

    Basic unit tests and functions.

--- pipelines/longterm/load.py
+++ pipelines/longterm/load.py
@@ -1,47 +1,88 @@
 """Module for loading long term data to S3."""
 
 from logging import getLogger
+from os import environ as ENV, path, walk, mkdir
+from shutil import rmtree
 
 from pandas import DataFrame
 from boto3 import client
 from dotenv import load_dotenv
-
-
-def get_s3_client() -> client:
-    """Return client to s3 bucket."""
-    logger = getLogger(__name__)
-    logger.info("Return S3 client")
-
-
-def load_to_s3(awsclient: client) -> bool:
-    """Load objects to s3."""
-    logger = getLogger(__name__)
-    logger.info("Starting load to S3...")
+from pyarrow import Table, parquet as pq
 
 
 def create_data_directory() -> bool:
     """Return true if data directory created successfully"""
     logger = getLogger(__name__)
     logger.info("Creating empty data directory for Parquet files...")
+    if not path.exists("data"):
+        mkdir("data")
+        mkdir("data/plant")
+        return True
+    return False
 
 
 def delete_data_directory() -> bool:
     """Return true if deleted data directory"""
     logger = getLogger(__name__)
     logger.info("Deleting filled data directory...")
+    rmtree("data")
+    if not path.exists("data"):
+        return False
+    return True
 
 
 def create_parquet(data: DataFrame) -> bool:
     """Save data as parquet files."""
     logger = getLogger(__name__)
     logger.info("Storing local parquet files...")
+    datatable = Table.from_pandas(data)
+    if not datatable:
+        logger.error("No data given.")
+        return False
+    pq.write_to_dataset(datatable, root_path="data/plant",
+                        partition_cols=["year", "month", "day", "plant_id"],
+                        basename_template="summary")
+    logger.info("Parquet created successfully.")
+    return True
+
+
+def get_s3_client() -> client:
+    """Return client to s3 bucket."""
+    logger = getLogger(__name__)
+    logger.info("Return S3 client")
+    return client("s3",
+                  aws_access_key_id=ENV["AWS_ACCESS_KEY_ID"],
+                  aws_secret_access_key=ENV["AWS_SECRET_ACCESS_KEY"])
+
+
+def load_to_s3(awsclient: client) -> bool:
+    """Load objects to s3."""
+    logger = getLogger(__name__)
+    logger.info("Starting load to S3...")
+    has_data = False
+    for root, _, files in walk("data"):
+        if files:
+            has_data = True
+            for file in files:
+                full_path = path.join(root, file)
+                logger.info("Uploading file: %s", full_path)
+                awsclient.upload_file(full_path,
+                                      ENV["S3_BUCKET"], f"input/{root[5:]}/{file}")
+    return has_data
+
+
+def load_all(df: DataFrame):
+    """Load all data given to S3."""
+    create_data_directory()
+    if create_parquet(df):
+        s3 = get_s3_client()
+        load_to_s3(s3)
+    delete_data_directory()
 
 
 if __name__ == "__main__":
     load_dotenv()
-    sample_dataframe = DataFrame()
-    delete_data_directory()
-    create_data_directory()
-    if create_parquet(sample_dataframe):
-        s3 = get_s3_client()
-        load_to_s3(s3)
+    sample_dataframe = DataFrame({"plant_id": [1, 2, 3],
+                                  "name": ["colin", "kevin", "sam"],
+                                  "botanist": [1, 2, 1]})
+    load_all(sample_dataframe)
--- pipelines/longterm/test_load.py
+++ pipelines/longterm/test_load.py
@@ -1,6 +1,6 @@
 """Tests for long term load module."""
 
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, call
 from pytest import mark
 
 from load import (create_data_directory,
@@ -9,21 +9,24 @@ from load import (create_data_directory,
                   load_to_s3)
 
 
-@mark.parametrize("success", (True, False))
-def test_create_data_directory(success):
+@mark.parametrize("exists", (True, False))
+def test_create_data_directory(exists):
     """Test that method attempts to create directory."""
-    with patch("path.exists") as mock_exists:
+    with patch("load.path.exists") as mock_exists:
         with patch("load.mkdir") as mock_mkdir:
-            mock_exists.return_value = success
+            mock_exists.return_value = exists
             actual = create_data_directory()
-            mock_mkdir.assert_called_once_with("data")
-    assert actual == success
+            if exists:
+                mock_mkdir.assert_not_called()
+            else:
+                mock_mkdir.assert_has_calls([call("data"), call("data/plant")])
+    assert actual != exists
 
 
 @mark.parametrize("success", (False, True))
 def delete_data_directory(success):
     """Test that method attempts to delete directory."""
-    with patch("path.exists") as mock_exists:
+    with patch("load.path.exists") as mock_exists:
         with patch("load.rmtree") as mock_rmtree:
             mock_exists.return_value = success
             actual = delete_data_directory()
@@ -34,19 +37,25 @@ def delete_data_directory(success):
 def test_create_parquet():
     """Test that create parquet attempts to make parquet files."""
     mock_dataframe = Mock()
-    mock_dataframe.to_parquet.return_value = None
-    actual = create_parquet(mock_dataframe)
-    mock_dataframe.to_parquet.assert_called()
-    assert actual
+    mock_table_instance = Mock()
+    with patch("load.Table") as mock_table:
+        with patch("load.pq.write_to_dataset") as mock_write:
+            mock_table.from_pandas.return_value = mock_table_instance
+            actual = create_parquet(mock_dataframe)
+            mock_table.from_pandas.assert_called()
+            mock_write.assert_called()
+            assert actual
 
 
-@mark.parametrize("directory", ([["summary", "summary"], "", ["test/plant_1", "test/plant_2"]],
-                                [["093725y1792", "0823528162ef"], "", ["test/plant_1", "test/plant_2"]]))
+@mark.parametrize("directory", ((("test/planyt_1", "", "summary"), ("test/plant_2", "", "summary")),
+                                (("test/planyt_1", "", "hs766sgvhba7d"), ("test/plant_2", "", "hs766sgvhba7d"))))
 def test_load_s3(directory):
     """Test that load to S3 attempts to contact client with formatted arguments."""
     mock_client = Mock()
     mock_client.upload_file.return_value = None
+    print(directory)
     with patch("load.walk") as mock_walk:
-        mock_walk.return_value = directory
-        load_to_s3(mock_client)
-        mock_client.upload_file.assert_called()
+        with patch("load.ENV") as mock_environ:
+            mock_walk.return_value = directory
+            load_to_s3(mock_client)
+            mock_client.upload_file.assert_called()
