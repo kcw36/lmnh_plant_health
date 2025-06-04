c491c7bfd2929cdf607ed6603f342dc2e953022e
kcw36 <kylewat2000@hotmail.com>
2025-06-04 13:50:35 +0100

    Add extract test imports

--- pipelines/longterm/test_extract.py
+++ pipelines/longterm/test_extract.py
@@ -1,1 +1,6 @@
 """Tests for extract module."""
+
+
+from extract import (get_data_from_rds, get_dataframe_from_dict,
+                     get_dict_from_rows, get_full_data, get_schema,
+                     truncate_record)
