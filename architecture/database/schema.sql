6b98337a5a097247008a63868efc5c7b1b8b54f7
AhmetCengiz03 <cengizahmete@gmail.com>
2025-06-05 11:04:35 +0100

    Update data types

--- architecture/database/schema.sql
+++ architecture/database/schema.sql
@@ -15,51 +15,51 @@ DROP TABLE IF EXISTS origin_country;
 GO
 
 CREATE TABLE origin_country (
-    country_id INT IDENTITY(1,1) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    country_id TINYINT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 GO
 
 CREATE TABLE botanist (
-    botanist_id INT IDENTITY(1,1) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    phone VARCHAR(50) NOT NULL
+    botanist_id TINYINT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) NOT NULL
 );
 GO
 
 CREATE TABLE origin_city (
     city_id INT IDENTITY(1,1) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    country_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    country_id TINYINT NOT NULL,
     FOREIGN KEY (country_id) REFERENCES origin_country(country_id)
 );
 GO
 
 CREATE TABLE plant (
-    plant_id INT PRIMARY KEY,  -- Manual input
-    name VARCHAR(255) NOT NULL,
+    plant_id TINYINT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     city_id INT NOT NULL,
     FOREIGN KEY (city_id) REFERENCES origin_city(city_id)
 );
 GO
 
 CREATE TABLE botanist_plant (
-    botanist_plant_id INT IDENTITY(1,1) PRIMARY KEY,
-    plant_id INT NOT NULL,
-    botanist_id INT NOT NULL,
+    botanist_plant_id SMALLINT IDENTITY(1,1) PRIMARY KEY,
+    plant_id TINYINT NOT NULL,
+    botanist_id TINYINT NOT NULL,
     FOREIGN KEY (plant_id) REFERENCES plant(plant_id),
     FOREIGN KEY (botanist_id) REFERENCES botanist(botanist_id)
 );
 GO
 
 CREATE TABLE record (
-    record_id INT IDENTITY(1,1) PRIMARY KEY,
+    record_id BIGINT IDENTITY(1,1) PRIMARY KEY,
     temperature FLOAT,
     last_watered DATETIME,
     soil_moisture FLOAT,
     recording_taken DATETIME,
-    plant_id INT NOT NULL,
+    plant_id TINYINT NOT NULL,
     FOREIGN KEY (plant_id) REFERENCES plant(plant_id)
 );
 GO
