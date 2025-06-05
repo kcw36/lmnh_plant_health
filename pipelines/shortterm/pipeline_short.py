230484f0528a17e65dccc908374a5b8aecafb5a9
bxrkxnt <berkantdoguer@gmail.com>
2025-06-05 14:23:32 +0100

    Call function changes due to load script changes

--- pipelines/shortterm/pipeline_short.py
+++ pipelines/shortterm/pipeline_short.py
@@ -1,28 +1,48 @@
 """Script to run the full short term pipeline."""
+from sys import stdout
+from logging import getLogger, StreamHandler, INFO
 
 from transform_short import transform_data
-from load_short import (
-    get_connection,
-    insert_origin_country,
-    insert_botanist,
-    insert_origin_city,
-    insert_plant,
-    insert_botanist_plant
-)
+from load_short import get_connection, load_data
+
+
+def set_logger():
+    """Set logger."""
+    logger = getLogger()
+    logger.setLevel(INFO)
+    logger.addHandler(StreamHandler(stdout))
 
 
 def run_pipeline():
     """Runs the pipeline."""
 
+    logger = getLogger()
+    logger.info("Attempting")
+
     clean_df = transform_data()
 
     with get_connection as conn:
-        insert_origin_country(clean_df, conn)
-        insert_botanist(clean_df, conn)
-        insert_origin_city(clean_df, conn)
-        insert_plant(clean_df, conn)
-        insert_botanist_plant(clean_df, conn)
+        load_data(clean_df, conn)
+
+
+def lambda_handler():
+    """AWS handler for short-term ETL."""
+
+    set_logger()
+
+    logger = getLogger()
+    logger.info("Initiating short-term ETL with Lambda..")
+
+    try:
+        run_pipeline()
+        return {
+            "statusCode": 200,
+            "message": "Short-term ETL pipeline completed."
+        }
+    except Exception as e:
+        logger.error("Short-term ETL pipeline failed: %s", str(e))
 
 
 if __name__ == "__main__":
+    set_logger()
     run_pipeline()
