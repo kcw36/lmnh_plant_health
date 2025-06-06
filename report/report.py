b0ee88a9a5401eb32d437d4de38f07b957d12a70
daynyus02 <daynyus.halinauskas02@gmail.com>
2025-06-06 12:18:02 +0100

    Producing critical plants

--- report/report.py
+++ report/report.py
@@ -1,9 +1,13 @@
 """Report script that notifies botanist if there is something wrong with a plant."""
 
 from dotenv import load_dotenv
-from data import get_connection
+from data import (get_connection,
+                  get_latest_readings,
+                  identify_critical_plants,
+                  )
 
 if __name__ == "__main__":
     load_dotenv()
     with get_connection() as conn:
-        print("Connected successfully")
+        critical_plants = identify_critical_plants(conn)
+        print(critical_plants)
